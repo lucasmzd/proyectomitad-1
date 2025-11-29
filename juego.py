bcb5dea1f2a08c515bcab908afd66a55f74e28b5
lucasmzd <ilucasmzd@gmail.com>
2025-11-29 13:28:57 -0300

    Categorias implementadas

--- juego.py
+++ juego.py
@@ -28,31 +28,34 @@ def actualizar_tiempo(datos_juego):
     datos_juego["tiempo_restante"] = max(0, tiempo_cfg - transcurrido)
     if datos_juego["tiempo_restante"] <= 0:
         datos_juego["cantidad_vidas"] -= 1
-        pasar_pregunta(datos_juego, lista_preguntas)
+        pasar_pregunta(datos_juego, datos_juego["preguntas_filtradas"])
         iniciar_tiempo_pregunta(datos_juego)
 
 def mostrar_juego(pantalla: pygame.Surface, cola_eventos, datos_juego, categoria_elegida):
     ventana = "juego"
+    if "preguntas_filtradas" not in datos_juego:
+        datos_juego["preguntas_filtradas"] = [
+            p for p in lista_preguntas if p["categoria"] == categoria_elegida.lower()]
+        datos_juego["pregunta_actual"] = 0
     cuadro_pregunta = crear_elemento_juego("texturas/textura_pregunta.jpg", ANCHO_PREGUNTA, ALTO_PREGUNTA, 80, 100)
     lista_respuestas = crear_lista_respuestas("texturas/textura_respuesta.jpg", 125, 260, 4)
-    pregunta_actual = obtener_pregunta_actual(datos_juego, lista_preguntas)
+    pregunta_actual = obtener_pregunta_actual(datos_juego, datos_juego["preguntas_filtradas"])
     if "inicio_pregunta" not in datos_juego:
         iniciar_tiempo_pregunta(datos_juego)
     actualizar_tiempo(datos_juego)
-    # EVENTOS
+
     for evento in cola_eventos:
         if evento.type == pygame.MOUSEBUTTONDOWN and evento.button == 1:
             procesado = responder_pregunta_pygame(
-                lista_respuestas,
-                evento.pos,
-                SONIDO_CLICK,
-                datos_juego,
-                lista_preguntas,
-                pregunta_actual
+            lista_respuestas,
+            evento.pos,
+            SONIDO_CLICK,
+            datos_juego,
+            datos_juego["preguntas_filtradas"],
+            pregunta_actual
             )
-
             if procesado:
-                pregunta_actual = obtener_pregunta_actual(datos_juego, lista_preguntas)
+                pregunta_actual = obtener_pregunta_actual(datos_juego, datos_juego["preguntas_filtradas"])
                 cuadro_pregunta = crear_elemento_juego("texturas/textura_pregunta.jpg", ANCHO_PREGUNTA, ALTO_PREGUNTA, 80, 100)
                 lista_respuestas = crear_lista_respuestas("texturas/textura_respuesta.jpg", 125, 260, 4)
     if datos_juego.get("cantidad_vidas", 0) <= 0:
